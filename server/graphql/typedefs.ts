1ac494829b1b58bfc08c2c014b8d5eebffdb3d35
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-08-10 21:15:36 +0800

    user resolvers + project resolvers + models

---
+++ server/graphql/typedefs.ts
@@ -0,0 +1,89 @@
+const { gql } = require('apollo-server')
+
+module.exports = gql`
+    type User {
+        _id: ID
+        name: String
+        password: String
+        ticketIds: [ID]
+        projectIds: [ID]
+        position: String
+        token: String
+    }
+
+    type Roles {
+        _id: ID
+        userId: ID
+        role: Int
+    }
+
+    type Comment {
+        _id: ID
+        userId: ID
+        description: String
+        createdAt: String
+    }
+
+    type Project {
+        _id: ID
+        createdBy: String
+        title: String
+        members: [Roles]
+        ticketIds: [ID]
+        comments: [Comment]
+        createdAt: String
+        code: String
+    }
+
+    type Ticket {
+        _id: ID
+        description: String
+        projectId: ID
+        comments: [Comment]
+        handlers: [ID]
+        status: String
+        priority: String
+        createdAt: String
+    }
+
+    type Query {
+        getUsers: [User]
+        getUser(_id: ID!): User
+        getProjects: [Project]
+        getProject(_id: ID!): Project
+        getTickets: [Ticket]
+        getTicket(_id: ID!): Ticket
+    }
+
+    type Mutation {
+        createUser(name: String!, password: String!, position: String!): User
+
+        createProject(id: ID!, title: String!, members: [ID]!): Project!
+        editProject(id: ID!, title: String): Project!
+        addMember(projectId: ID!, userId: ID!, role: String!): Project!
+        removeMember(projectId: ID!, userId: ID!): Project!
+        joinProject(projectId: ID!, userId: ID!, code: String!): Project!
+        commentProject(projectId: ID!, body: String!, userId: ID!): Comment!
+        deleteCommentProject(
+            projectId: ID!
+            commentId: ID!
+            userId: ID!
+        ): Comment!
+        deleteProject(projectId: ID!, userId: ID!): Boolean!
+
+        createTicket(
+            projectId: ID!
+            title: String!
+            userId: ID!
+            status: String!
+            priority: String!
+        ): Ticket!
+        editTicket(ticketId: ID!, title: String!): Ticket!
+        commentTicket(ticketId: ID!, body: String!, userId: String!): Comment!
+        deleteCommentTicket(ticketId: ID!, commentId: ID!): Boolean!
+        assignTicket(ticketId: ID!, assigner: ID!, assignee: ID!): Ticket!
+        ownTicket(ticketId: ID!, userId: ID!): Ticket!
+    }
+`
+
+//Project.members should use Roles type ids
