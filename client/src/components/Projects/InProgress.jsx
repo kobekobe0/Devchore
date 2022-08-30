8e6158dc33e846a573a6613a955d44813d068a84
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-08-30 15:21:31 +0800

    Inprogess grid

---
+++ client/src/components/Projects/InProgress.jsx
@@ -0,0 +1,40 @@
+import TicketCard from './TicketCard'
+
+const tasks = [
+    {
+        description: 'Create a new listing for the said items',
+        projectId: '1231209dsndasd',
+        comments: [],
+        handlers: ['user1ID', 'user2ID'],
+        title: 'Create a new listing for the said items',
+        createdBy: 'user1ID',
+        status: 'TODO',
+        priority: 'MID',
+        createdAt: 'May 15, 2022',
+    },
+    {
+        description: 'reduce the latency used by the server',
+        projectId: '1231209dsssndasd',
+        comments: [],
+        handlers: ['user1ID'],
+        title: 'reduce the latency used by the server',
+        createdBy: 'user2ID',
+        status: 'TODO',
+        priority: 'LOW',
+        createdAt: 'May 16, 2022',
+    },
+]
+
+function InProgress() {
+    return (
+        <div className="flex flex-col w-full items-center mt-4">
+            {tasks.map((task) => (
+                <div className="w-full flex flex-col items-center">
+                    <TicketCard details={task} />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default InProgress
