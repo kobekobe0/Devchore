644c5ff6a209072d80c4ab46518f7c59f16f96f3
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-08-30 19:20:21 +0800

    update status button

--- client/src/components/Projects/TicketCard.jsx
+++ client/src/components/Projects/TicketCard.jsx
@@ -1,8 +1,10 @@
 import { MdSystemUpdateAlt } from 'react-icons/md'
 import p1 from '../../imgs/p1.jpg'
 import { BiCommentDetail } from 'react-icons/bi'
+import { useState } from 'react'
 
 function TicketCard({ details }) {
+    const [show, setShow] = useState(false)
     return (
         <div className="bg-white mb-7 rounded-3xl shadow-lg w-4/5 flex items-center justify-center">
             <div className="flex flex-col w-full p-5">
@@ -10,9 +12,46 @@ function TicketCard({ details }) {
                     <h1 className="text-2xl font-semibold text-gray-700">
                         {details.title}
                     </h1>
-                    <button className="text-xs border bg-indigo-400 border-indigo-500 py-1  px-2 ml-5 rounded-xl h-fit">
-                        <MdSystemUpdateAlt style={{ color: 'white' }} />
-                    </button>
+                    <div className="flex justify-center">
+                        <button
+                            onClick={() => setShow(!show)}
+                            className="text-xs border bg-indigo-400 border-indigo-500 py-1  px-2 ml-5 rounded-xl h-fit"
+                        >
+                            <MdSystemUpdateAlt style={{ color: 'white' }} />
+                        </button>
+                        <div
+                            id="dropdown"
+                            className={` ${
+                                show ? 'block absolute' : 'hidden absolute'
+                            }z-10 w-40 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-200 opacity-90 absolute mt-8`}
+                        >
+                            <ul
+                                className="py-1 text-sm text-black dark:text-gray-800 cursor-pointer"
+                                aria-labelledby="dropdownDefault"
+                            >
+                                <li onClick={() => setShow(false)}>
+                                    <a className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                        To Do
+                                    </a>
+                                </li>
+                                <li onClick={() => setShow(false)}>
+                                    <a className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                        In Progress
+                                    </a>
+                                </li>
+                                <li onClick={() => setShow(false)}>
+                                    <a className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                        To Review
+                                    </a>
+                                </li>
+                                <li onClick={() => setShow(false)}>
+                                    <a className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                        Finished
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
                 <div
                     className={`${details.priority == 'HIGH' && 'bg-red-400'} ${
