de766ad0a97ee4a5719928d7227159da287cf640
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-08-31 08:48:05 +0800

    render login and register

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,28 @@ import Navbar from './components/Nav/Navbar'
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 import ProgressBar from './components/ProgressBar/ProgressBar'
 import Projects from './components/Projects/Projects'
+import Login from './components/Landing/Login'
+import Register from './components/Landing/Register'
+
+function Home() {
+    return (
+        <>
+            <Navbar />
+            <ProgressBar />
+            <Projects />
+        </>
+    )
+}
 
 function App() {
     return (
         <Router>
             <div className="flex w-full h-full flex-col">
-                <Navbar />
-                <ProgressBar />
-                <Projects />
-                <Routes></Routes>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
             </div>
         </Router>
     )
