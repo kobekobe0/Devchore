40b5c024c3539afaf449c28ceb7e1cdae44d82bf
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-08-31 11:03:27 +0800

    login resolver + fix register user

--- server/graphql/resolvers/user.ts
+++ server/graphql/resolvers/user.ts
@@ -1,5 +1,12 @@
 const UserModel = require(`../../model/user.model`)
 
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
+const jwt = require('jsonwebtoken')
+
+const secretKey = 'retardedtypeofthoughts'
+
 module.exports = {
     Query: {
         async getUsers() {
@@ -15,17 +22,63 @@ module.exports = {
             {
                 name,
                 password,
+                confirmPassword,
                 position,
-            }: { name: string; password: string; position: string }
+            }: {
+                name: string
+                password: string
+                confirmPassword: string
+                position: string
+            }
         ) {
             try {
+                const exists = await UserModel.findOne({ name: name })
+                if (exists) throw new Error('Someone already uses this name')
+
+                if (password != confirmPassword)
+                    throw new Error('Password did not match')
+
+                const salt = bcrypt.genSaltSync(saltRounds)
+                const hash = bcrypt.hashSync(password, salt)
+
+                if (!hash || !salt)
+                    throw new Error('Something went wrong hashing password')
+
                 const user = new UserModel({
                     name,
-                    password,
+                    password: hash,
                     position,
                 })
                 await user.save()
                 return user
+            } catch (e) {
+                throw new Error(e)
+            }
+        },
+
+        async login(
+            _: null,
+            { name, password }: { name: string; password: string }
+        ) {
+            try {
+                const user = await UserModel.findOne({ name: name })
+                if (!user) throw new Error("User didn't exists")
+
+                const match = await bcrypt.compare(password, user.password)
+                if (!match) throw new Error("Credentials didn't match")
+                if (match) {
+                    console.log('ff')
+                    const token = jwt.sign(
+                        {
+                            _id: user._id,
+                            name: user.name,
+                        },
+                        secretKey
+                    )
+                    console.log('gg')
+                    console.log(token)
+                    return token
+                }
             } catch (e) {
                 throw new Error(e)
             }
