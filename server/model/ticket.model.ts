f4eee40b8854b4af1f9f59b792ad6d44cb689e3c
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-09-02 21:51:41 +0800

    addded 'deleted' field + change handlers field

--- server/model/ticket.model.ts
+++ server/model/ticket.model.ts
@@ -8,12 +8,13 @@ const Ticket = new Schema({
         type: [{ userId: String, body: String, createdAt: String }],
         default: [],
     },
-    handlers: { type: [String], default: [] },
+    handlers: { type: [String], required: true },
     title: { type: String, required: true },
     createdBy: { type: String, required: true },
     status: { type: String, required: true },
     priority: { type: String, required: true },
     createdAt: { type: String, required: true },
+    deleted: { type: Boolean, default: false },
 })
 
 module.exports = model('Ticket', Ticket)
