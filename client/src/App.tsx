887cbedde17b2e4c1380cf1fd8cdc2d37364588e
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-09-04 16:10:36 +0800

    Apollo client configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ import ProgressBar from './components/ProgressBar/ProgressBar'
 import Projects from './components/Projects/Projects'
 import Login from './components/Landing/Login'
 import Register from './components/Landing/Register'
+import { useEffect } from 'react'
+import ProtectedRoutes from './components/ProtectedRoutes'
 
 function Home() {
     return (
@@ -17,11 +19,19 @@ function Home() {
 }
 
 function App() {
+    const hasJwt = () => {
+        let flag = false
+
+        localStorage.getItem('token') ? (flag = true) : (flag = false)
+        return flag
+    }
     return (
         <Router>
             <div className="flex w-full h-full flex-col">
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route element={<ProtectedRoutes />}>
+                        <Route path="/" element={<Home />} />
+                    </Route>
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                 </Routes>
